42b136f7fc7a538c88470ab84d40d4464d4003ab
LucasA <37451939+lucasaraujonrt@users.noreply.github.com>
2020-04-25 15:19:28 -0300

    https://github.com/Projeto-PES/Projeto-Pes.git

---
+++ src/componente/Tela Principal/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+// import { Container } from './styles';
+
+export default function TelaPrincipal() {
+  return (
+    <div className="all">
+      <section>
+        <div className="handlerinputLogin">
+          <input className="Login" type="text" />
+          <input className="Password" type="text" />
+        </div>
+      </section>
+    </div>
+  );
+}
