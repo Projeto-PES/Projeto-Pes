78d96ea41e78662298fd681205381da535454c83
Ciro-Lopes <ciro.flopes@gmail.com>
2020-05-10 16:51:03 -0300

    Criacao do main

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./global.css";
 import Logon from "./pages/Logon";
+import Main from "./pages/Main";
 
 function App() {
-  return <Logon></Logon>;
+  return (<Logon></Logon>,
+      <Main></Main>);
 }
 export default App;
---
+++ src/pages/Main/index.js
@@ -0,0 +1,6 @@
+import React from "./node_modules/react";
+import "./styles.css";
+
+export default function Main(){
+    return <h1>Qualquer Coisa</h1>
+}
