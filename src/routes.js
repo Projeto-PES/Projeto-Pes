ee03a9a20a76b57aeb219688a52ddbb53e23ea75
Ciro-Lopes <ciro.flopes@gmail.com>
2020-05-10 17:30:09 -0300

    Rota main criada

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Logon from "./pages/Logon";
+import Main from "./pages/Main";
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Logon} />
+        <Route path="/main" component={Main} />
       </Switch>
     </BrowserRouter>
   );
